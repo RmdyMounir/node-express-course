cadec427bd618c3055ac6b6cb06322f867075824
RmdyMounir <mounirramadany738@gmail.com>
2020-09-10 11:35:13 +0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 
@@ -9,13 +10,7 @@ const mockUserData=[
 {name:'Mounir'},
 {name:'Jojo'},
 ]
-app.get('/user', function(req,res){
-	res.json({
-		  success: true,
-	          message: 'successfully got users. Nice!',
-		  users: mockUserData
-		})
-})
+
 
 apt.get ('/user/:id',function(req,res){
 		console .log(req.paprams.id)
